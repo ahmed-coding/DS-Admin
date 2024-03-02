6fdc690ec9d8ba2208d04236e5b1811391824692
Ahmed M Hamza <71948774+ahmed-coding@users.noreply.github.com>
2024-03-02 06:49:44 +0300

    update .env files

--- config/paystack.php
+++ config/paystack.php
@@ -15,24 +15,24 @@ return [
      * Public Key From Paystack Dashboard
      *
      */
-    'publicKey' => getenv('PAYSTACK_PUBLIC_KEY','pk_test_984c15fc89e28647c149e49654c69475ef565eaf'),
+    'publicKey' => getenv('PAYSTACK_PUBLIC_KEY', 'pk_test_984c15fc89e28647c149e49654c69475ef565eaf'),
 
     /**
      * Secret Key From Paystack Dashboard
      *
      */
-    'secretKey' => getenv('PAYSTACK_SECRET_KEY','sk_test_77556985d455a0fd326da6662273ad1c3eb8f097'),
+    'secretKey' => getenv('PAYSTACK_SECRET_KEY'),
 
     /**
      * Paystack Payment URL
      *
      */
-    'paymentUrl' => getenv('PAYSTACK_PAYMENT_URL',"https://api.paystack.co"),
+    'paymentUrl' => getenv('PAYSTACK_PAYMENT_URL', "https://api.paystack.co"),
 
     /**
      * Optional email address of the merchant
      *
      */
-    'merchantEmail' => getenv('MERCHANT_EMAIL','showrov2185@gmail.com'),
+    'merchantEmail' => getenv('MERCHANT_EMAIL', 'showrov2185@gmail.com'),
 
 ];
